316c6ba755496881ed512abdd874a02e8a54a793
bcons009 <bcons009@fiu.edu>
2021-03-19 14:27:43 -0400

    Created the first version of DirectedGraph.cs. Includes the classes DirectedGraph, Node, and Edge. Early documentation was also added to some methods of the Node class.

---
+++ DealerOn Trains Problem/DirectedGraph.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DealerOn_Trains_Problem
+{
+    public class Edge : IEquatable<Edge>
+    {
+        public Node node { get; set; }
+
+        public int weight { get; set; }
+
+        public override int GetHashCode()
+        {
+            return node.GetName();
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) 
+                return false;
+            Edge objAsEdge = obj as Edge;
+            if (objAsEdge == null) 
+                return false;
+            else 
+                return Equals(objAsEdge);
+        }
+        public bool Equals(Edge other)
+        {
+            if (other == null) 
+                return false;
+            return (this.node.GetName().Equals(other.node.GetName()));
+        }
+    }
+
+    public class Node
+    {
+        private char name;
+        private List<Edge> edges;
+
+        public Node(char name)
+        {
+            this.name = name;
+            edges = new List<Edge>();
+        }
+
+        public char GetName()
+        {
+            return name;
+        }
+
+        // Add a new edge leading out from the node to another node.
+        // Returns false if the user tries to add an edge to the same node
+            // (creating a single-node loop).
+        // If the user tries to add an edge to a node that already has an edge 
+            // from the current node, the node's weight will be updated with
+            // the new weight specified in the argument int weight.
+        public bool AddEdge(Node n, int weight)
+        {
+            if (n.GetName() == name)
+            {
+                return false;
+            }
+            foreach (Edge e in edges)
+            {
+                if (n.GetName() == e.node.GetName())
+                {
+                    e.weight = weight;
+                    return true;
+                }
+            }
+
+            edges.Add(new Edge() { node = n, weight = weight});
+            return true;
+        }
+
+        // Returns the Edge between the current Node and the Node
+            // specified in the argument Node n.
+        // Returns null if there is no Edge found between the current
+            // Node and Node n.
+        public Edge FindEdgeTo(Node n)
+        {
+            foreach (Edge e in edges)
+            {
+                if (n.GetName() == e.node.GetName())
+                    return e;
+            }
+            return null;
+        }
+    }
+
+	public class DirectedGraph
+	{
+		private List<Node> nodes;
+
+		public DirectedGraph()
+		{
+			nodes = new List<Node>();
+		}
+
+		public bool AddNode(char name)
+        {
+			if (GetNode(name) == null)
+			{
+				nodes.Add(new Node(name));
+				return true;
+			}
+			else return false;
+        }
+
+        public Node GetNode(char name)
+        {
+			foreach(Node n in nodes)
+            {
+				if(n.GetName() == name)
+					return n;
+            }
+			return null;
+        }
+	}
+}
--- DealerOn Trains Problem/Program.cs
+++ DealerOn Trains Problem/Program.cs
@@ -2,25 +2,37 @@
 
 namespace DealerOn_Trains_Problem
 {
+
+    // Need two additional classes:
+        // DirectedGraph (object, the directed graph itself)
+        // OutputHandler (static class, handles all 10 output cases)
+
     class Program
     {
-        // DirectedMap CreateMap( text input from .txt )
+        // DirectedGraph CreateMap( text input from .txt )
             // Splits input by ', ' and stores split into array/list
-            // Initializes new DirectedMap object
+            // Initializes new DirectedGraph object
             // Iterates through input array to add new nodes and/or edges
-            // returns a new DirectedMap object
+            // returns a new DirectedGraph object
 
-        // void OutputFunc(DirectedMap map)
-            // 
+        // void OutputCalls(DirectedGraph map)
+            // make 10 calls to the static "OutputHandler" class, one for each test case
 
+        // move to static "OutputHandler" class
         void Print(int n, string result) {
             string output = "Output #" + n + ": " + result;
             Console.WriteLine(output);
         }
 
+
+        // Make sure to add line warning about auto-closing console window
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // read input.txt, store as string (input_s)
+            // DirectedGraph trainMap = CreateMap(input_s);
+            // OutputCalls(trainMap);
         }
     }
 }
