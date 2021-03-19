e2091487961b08435d67954f26720540de1cc3f3
bcons009 <bcons009@fiu.edu>
2021-03-19 12:16:23 -0400

    Add project files.

---
+++ DealerOn Trains Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DealerOn_Trains_Problem
+{
+    class Program
+    {
+        // DirectedMap CreateMap( text input from .txt )
+            // Splits input by ', ' and stores split into array/list
+            // Initializes new DirectedMap object
+            // Iterates through input array to add new nodes and/or edges
+            // returns a new DirectedMap object
+
+        // void OutputFunc(DirectedMap map)
+            // 
+
+        void Print(int n, string result) {
+            string output = "Output #" + n + ": " + result;
+            Console.WriteLine(output);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
