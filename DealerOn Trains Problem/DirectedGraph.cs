9d05b38d3d608ee246a6948066dfa9e31990fc31
bcons009 <bcons009@fiu.edu>
2021-03-19 14:58:30 -0400

    Updated documentation to the classes in DirectedGraph.cs

--- DealerOn Trains Problem/DirectedGraph.cs
+++ DealerOn Trains Problem/DirectedGraph.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 
 namespace DealerOn_Trains_Problem
 {
+    /// <summary>
+    /// A class for an object that represents an edge in a directed graph.
+    /// Each edge contains an edge weight and a Node object that the edge is pointing to.
+    /// </summary>
+    /// <remarks>
+    /// Includes overriden Equals and GetHashCode functions for comparing Edge objects,
+    /// used when creating List<Edge>() objects.
+    /// </remarks>
     public class Edge : IEquatable<Edge>
     {
         public Node node { get; set; }
@@ -31,9 +39,16 @@ namespace DealerOn_Trains_Problem
         }
     }
 
+    /// <summary>
+    /// A class for an object that represents a node in a directed graph.
+    /// Includes the node's name and a list of all the edges pointing outward from the node.
+    /// Also includes setter and getter functions for edges in the edge list.
+    /// </summary>
     public class Node
     {
+        /// <value>The character representing the node's name</value>
         private char name;
+        /// <value>The list of edges pointing outward from the node.</value>
         private List<Edge> edges;
 
         public Node(char name)
@@ -47,12 +62,19 @@ namespace DealerOn_Trains_Problem
             return name;
         }
 
-        // Add a new edge leading out from the node to another node.
-        // Returns false if the user tries to add an edge to the same node
-            // (creating a single-node loop).
-        // If the user tries to add an edge to a node that already has an edge 
-            // from the current node, the node's weight will be updated with
-            // the new weight specified in the argument int weight.
+        /// <summary>
+        /// Add a new edge leading out from the node to another node.
+        /// </summary>
+        /// <remarks>
+        /// Returns false if the user tries to add an edge to the same node
+        /// (creating a single-node loop). If the user tries to add an edge to 
+        /// a node that already has an edge from the current node, the node's weight
+        /// will be updated with the new weight specified in the argument int weight.
+        /// </remarks>
+        /// <param name="n">A Node object that will be connected 
+        /// to the current node via the new edge</param>
+        /// <param name="weight">A number representing the weight of the new edge</param>
+        /// <returns>Boolean; returns true if an edge was created or updated, otherwise false.</returns>
         public bool AddEdge(Node n, int weight)
         {
             if (n.GetName() == name)
@@ -72,10 +94,17 @@ namespace DealerOn_Trains_Problem
             return true;
         }
 
-        // Returns the Edge between the current Node and the Node
-            // specified in the argument Node n.
-        // Returns null if there is no Edge found between the current
-            // Node and Node n.
+        /// <summary>
+        /// Finds and returns the Edge between the current Node and the 
+        /// Node specified in the argument Node n, if such an Edge exists.
+        /// </summary>
+        /// <param name="n">The Node object to search for when looking
+        /// through all the edges in the edges list.</param>
+        /// <returns>
+        /// Returns the Edge between the current Node and the Node
+        /// specified in the argument Node n. Returns null if there is no Edge 
+        /// found between the current Node and Node n.
+        /// </returns>
         public Edge FindEdgeTo(Node n)
         {
             foreach (Edge e in edges)
@@ -87,6 +116,11 @@ namespace DealerOn_Trains_Problem
         }
     }
 
+    /// <summary>
+    /// A class for an object that represents a directed graph. Contains a list
+    /// of Node objects representing all the nodes within the graph, as well as 
+    /// setter and getter methods for those nodes.
+    /// </summary>
 	public class DirectedGraph
 	{
 		private List<Node> nodes;
@@ -96,6 +130,15 @@ namespace DealerOn_Trains_Problem
 			nodes = new List<Node>();
 		}
 
+        /// <summary>
+        /// Creates a new Node object to add to the graph.
+        /// </summary>
+        /// <remarks>
+        /// Will not create a new node if the node name provided matches the name
+        /// of a pre-existing node.
+        /// </remarks>
+        /// <param name="name">A character representing the name of the new node.</param>
+        /// <returns>Boolean; returns true if the node was created, otherwise false.</returns>
 		public bool AddNode(char name)
         {
 			if (GetNode(name) == null)
@@ -106,6 +149,15 @@ namespace DealerOn_Trains_Problem
 			else return false;
         }
 
+        /// <summary>
+        /// Iterates through the list of Nodes to find a node with the same name as the character
+        /// provided as an argument.
+        /// </summary>
+        /// <param name="name">The name of the node to find within the nodes list.</param>
+        /// <returns>
+        /// The node with the same name as the param "name". If no such node is
+        /// found, returns null.
+        /// </returns>
         public Node GetNode(char name)
         {
 			foreach(Node n in nodes)
